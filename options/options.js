31992784be10de81a3f0deb702d796d4b2f43222
Crowdbotics <team@crowdbotics.com>
2023-04-27 14:35:27 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "vlv-40255",
+  url: "https://vlv-40255.botics.co",
+  api: "https://vlv-40255.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
